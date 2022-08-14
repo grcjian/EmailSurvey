5c0d95509442c001c47b38e4547e7be1eb7307fa
Tianyi Jian <jian0092@algonquinlive.com>
2022-08-13 21:25:28 -0400

    try debug with stripe 1

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,7 @@
 //figure out what set of credentials to return
 if(process.env.NODE_ENV === 'production'){
   //in production, return the prod set of keys
+  console.log(process.env.NODE_ENV);
   module.exports = require('./prod');
 }else{
   //in development, return the dev keys
