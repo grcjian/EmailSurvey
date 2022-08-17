2c078a8395a0985dadde499e335dc49c0331d300
Tianyi Jian <jian0092@algonquinlive.com>
2022-08-16 21:29:36 -0400

    Done with client side

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
-
+mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
 
 const app = express();  //generate an object of express
@@ -24,6 +25,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
